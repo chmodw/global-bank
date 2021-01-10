b3b18f0eff7720104d6a5d90a51e9d2a7674f151
chamodya wimansha <chamodyawimansha@gmail.com>
2021-01-10 10:03:36 +0530

    staff area index basic structure created. styles added

--- public/index.php
+++ public/index.php
@@ -1,14 +1,14 @@
-<!doctype html>
-
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <title></title>
-  <meta charset="utf-8">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Globe Bank International</title>
 </head>
 
 <body>
-
+  <h1>Globe Bank Coming soon</h1>
 </body>
 
 </html>
--- public/staff/index.php
+++ public/staff/index.php
@@ -3,12 +3,25 @@
 <html lang="en">
 
 <head>
-    <title></title>
+    <title>Globe Bank International</title>
     <meta charset="utf-8">
+    <link rel="stylesheet" media="all" href="../stylesheets/staff.css">
 </head>
 
 <body>
-
+    <header>
+        <h1>Globe Bank International Staff Area</h1>
+    </header>
+    <navigation>
+        <ul>
+            <li><a href="index.php">Menu</a></li>
+        </ul>
+    </navigation>
+    <div id="content">
+    </div>
+    <footer>
+        &copy; <?php echo date("Y"); ?> Globe Bank International
+    </footer>
 </body>
 
 </html>
