db2607a5ba7327f4e4065a5c9c0129e4a64e3017
chamodya wimansha <chamodyawimansha@gmail.com>
2021-01-10 18:19:30 +0530

    create subject form created

--- private/functions.php
+++ private/functions.php
@@ -1,23 +1,37 @@
 <?php
 
-function url_for($script_path) {
+function url_for($script_path)
+{
   // add the leading '/' if not present
-  if($script_path[0] != '/') {
+  if ($script_path[0] != '/') {
     $script_path = "/" . $script_path;
   }
   return WWW_ROOT . $script_path;
 }
 
-function u($string="") {
+function u($string = "")
+{
   return urlencode($string);
 }
 
-function raw_u($string="") {
+function raw_u($string = "")
+{
   return rawurlencode($string);
 }
 
-function h($string="") {
+function h($string = "")
+{
   return htmlspecialchars($string);
 }
 
-?>
+function error_404()
+{
+  header($_SERVER["SERVER_PROTOCOL"] . "404 Not Found");
+  exit();
+}
+
+function error_500()
+{
+  header($_SERVER["SERVER_PROTOCOL"] . "500 Internal Server Error");
+  exit();
+}
