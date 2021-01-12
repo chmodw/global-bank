a1e3526ffbada79569439b36323947ff846b17de
chamodya wimansha <chamodyawimansha@gmail.com>
2021-01-12 14:51:50 +0530

    edit subject function added

--- private/query_functions.php
+++ private/query_functions.php
@@ -4,52 +4,80 @@ function find_all_subjects()
 {
     global $db;
 
-    $sql = "SELECT * FROM subjects ORDER BY position ASC";
-    $res = mysqli_query($db, $sql);
-    confirm_result_set($res);
-    return $res;
+    $sql = "SELECT * FROM subjects ";
+    $sql .= "ORDER BY position ASC";
+    //echo $sql;
+    $result = mysqli_query($db, $sql);
+    confirm_result_set($result);
+    return $result;
 }
 
 function find_subject_by_id($id)
 {
     global $db;
 
-    $sql = "SELECT * FROM subjects WHERE id='" . $id . "'";
-    $res = mysqli_query($db, $sql);
-    confirm_result_set($res);
-    $subject = mysqli_fetch_assoc($res);
-    mysqli_free_result($res);
-    // returning assoc array;
-    return $subject;
-}
-
-function find_all_pages()
-{
-    global $db;
-
-    $sql = "SELECT * FROM pages ORDER BY position ASC";
-    $res = mysqli_query($db, $sql);
-    confirm_result_set($res);
-    return $res;
+    $sql = "SELECT * FROM subjects ";
+    $sql .= "WHERE id='" . $id . "'";
+    $result = mysqli_query($db, $sql);
+    confirm_result_set($result);
+    $subject = mysqli_fetch_assoc($result);
+    mysqli_free_result($result);
+    return $subject; // returns an assoc. array
 }
 
 function insert_subject($menu_name, $position, $visible)
 {
     global $db;
 
-    $sql = "INSERT INTO subjects (menu_name, position, visible) VALUES (";
+    $sql = "INSERT INTO subjects ";
+    $sql .= "(menu_name, position, visible) ";
+    $sql .= "VALUES (";
     $sql .= "'" . $menu_name . "',";
     $sql .= "'" . $position . "',";
-    $sql .= "'" . $visible . "')";
-
+    $sql .= "'" . $visible . "'";
+    $sql .= ")";
     $result = mysqli_query($db, $sql);
-
+    // For INSERT statements, $result is true/false
     if ($result) {
         return true;
     } else {
-        //Failed
+        // INSERT failed
         echo mysqli_error($db);
         db_disconnect($db);
         exit;
     }
+}
+
+function update_subject($subject)
+{
+    global $db;
+
+    $sql = "UPDATE subjects SET ";
+    $sql .= "menu_name='" . $subject['menu_name'] . "', ";
+    $sql .= "position='" . $subject['position'] . "', ";
+    $sql .= "visible='" . $subject['visible'] . "' ";
+    $sql .= "WHERE id='" . $subject['id'] . "' ";
+    $sql .= "LIMIT 1";
+
+    $result = mysqli_query($db, $sql);
+    // For UPDATE statements, $result is true/false
+    if ($result) {
+        return true;
+    } else {
+        // UPDATE failed
+        echo mysqli_error($db);
+        db_disconnect($db);
+        exit;
+    }
+}
+
+function find_all_pages()
+{
+    global $db;
+
+    $sql = "SELECT * FROM pages ";
+    $sql .= "ORDER BY subject_id ASC, position ASC";
+    $result = mysqli_query($db, $sql);
+    confirm_result_set($result);
+    return $result;
 }
--- public/staff/subjects/edit.php
+++ public/staff/subjects/edit.php
@@ -2,26 +2,26 @@
 
 require_once('../../../private/initialize.php');
 
-if(!isset($_GET['id'])) {
+if (!isset($_GET['id'])) {
   redirect_to(url_for('/staff/subjects/index.php'));
 }
 $id = $_GET['id'];
-$menu_name = '';
-$position = '';
-$visible = '';
 
-if(is_post_request()) {
+if (is_post_request()) {
 
   // Handle form values sent by new.php
 
-  $menu_name = $_POST['menu_name'] ?? '';
-  $position = $_POST['position'] ?? '';
-  $visible = $_POST['visible'] ?? '';
+  $subject = [];
+  $subject['id'] = $id;
+  $subject['menu_name'] = $_POST['menu_name'] ?? '';
+  $subject['position'] = $_POST['position'] ?? '';
+  $subject['visible'] = $_POST['visible'] ?? '';
 
-  echo "Form parameters<br />";
-  echo "Menu name: " . $menu_name . "<br />";
-  echo "Position: " . $position . "<br />";
-  echo "Visible: " . $visible . "<br />";
+  $result = update_subject($subject);
+  redirect_to(url_for('/staff/subjects/show.php?id=' . $id));
+} else {
+
+  $subject = find_subject_by_id($id);
 }
 
 ?>
@@ -39,13 +39,15 @@ if(is_post_request()) {
     <form action="<?php echo url_for('/staff/subjects/edit.php?id=' . h(u($id))); ?>" method="post">
       <dl>
         <dt>Menu Name</dt>
-        <dd><input type="text" name="menu_name" value="<?php echo h($menu_name); ?>" /></dd>
+        <dd><input type="text" name="menu_name" value="<?php echo h($subject['menu_name']); ?>" /></dd>
       </dl>
       <dl>
         <dt>Position</dt>
         <dd>
           <select name="position">
-            <option value="1"<?php if($position == "1") { echo " selected"; } ?>>1</option>
+            <option value="1" <?php if ($subject['position'] == "1") {
+                                echo " selected";
+                              } ?>>1</option>
           </select>
         </dd>
       </dl>
@@ -53,7 +55,9 @@ if(is_post_request()) {
         <dt>Visible</dt>
         <dd>
           <input type="hidden" name="visible" value="0" />
-          <input type="checkbox" name="visible" value="1"<?php if($visible == "1") { echo " checked"; } ?> />
+          <input type="checkbox" name="visible" value="1" <?php if ($subject['visible'] == "1") {
+                                                            echo " checked";
+                                                          } ?> />
         </dd>
       </dl>
       <div id="operations">
