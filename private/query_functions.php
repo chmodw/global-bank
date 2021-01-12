73ad76744489b7d4f7ad6396fcb548d284f60083
chamodya wimansha <chamodyawimansha@gmail.com>
2021-01-12 13:40:31 +0530

    code added to create new subject

--- private/query_functions.php
+++ private/query_functions.php
@@ -9,3 +9,47 @@ function find_all_subjects()
     confirm_result_set($res);
     return $res;
 }
+
+function find_subject_by_id($id)
+{
+    global $db;
+
+    $sql = "SELECT * FROM subjects WHERE id='" . $id . "'";
+    $res = mysqli_query($db, $sql);
+    confirm_result_set($res);
+    $subject = mysqli_fetch_assoc($res);
+    mysqli_free_result($res);
+    // returning assoc array;
+    return $subject;
+}
+
+function find_all_pages()
+{
+    global $db;
+
+    $sql = "SELECT * FROM pages ORDER BY position ASC";
+    $res = mysqli_query($db, $sql);
+    confirm_result_set($res);
+    return $res;
+}
+
+function insert_subject($menu_name, $position, $visible)
+{
+    global $db;
+
+    $sql = "INSERT INTO subjects (menu_name, position, visible) VALUES (";
+    $sql .= "'" . $menu_name . "',";
+    $sql .= "'" . $position . "',";
+    $sql .= "'" . $visible . "')";
+
+    $result = mysqli_query($db, $sql);
+
+    if ($result) {
+        return true;
+    } else {
+        //Failed
+        echo mysqli_error($db);
+        db_disconnect($db);
+        exit;
+    }
+}
--- public/staff/subjects/create.php
+++ public/staff/subjects/create.php
@@ -2,7 +2,7 @@
 
 require_once('../../../private/initialize.php');
 
-if(is_post_request()) {
+if (is_post_request()) {
 
   // Handle form values sent by new.php
 
@@ -10,12 +10,9 @@ if(is_post_request()) {
   $position = $_POST['position'] ?? '';
   $visible = $_POST['visible'] ?? '';
 
-  echo "Form parameters<br />";
-  echo "Menu name: " . $menu_name . "<br />";
-  echo "Position: " . $position . "<br />";
-  echo "Visible: " . $visible . "<br />";
+  $res = insert_subject($menu_name, $position, $visible);
+  $id = mysqli_insert_id($db);
+  redirect_to(url_for('/staff/subjects/show.php?id=' . $id));
 } else {
   redirect_to(url_for('/staff/subjects/new.php'));
 }
-
-?>
