642b3acb1ea5bbf2e388043352c56f3cdcaa9b7a
chamodya wimansha <chamodyawimansha@gmail.com>
2021-01-15 07:37:14 +0530

    pages added under the subjects in the nav

--- private/shared/public_navigation.php
+++ private/shared/public_navigation.php
@@ -6,6 +6,18 @@
         <a href="<?php echo url_for('index.php'); ?>">
           <?php echo h($nav_subject['menu_name']); ?>
         </a>
+        <?php $nav_pages = find_pages_by_subject_id($nav_subject["id"]); ?>
+        <ul class="pages">
+          <?php while ($nav_page = mysqli_fetch_assoc($nav_pages)) { ?>
+            <li>
+              <a href="<?php echo url_for('index.php'); ?>">
+                <?php echo h($nav_page['menu_name']); ?>
+              </a>
+            </li>
+          <?php } // while $nav_pages 
+          ?>
+        </ul>
+        <?php mysqli_free_result($nav_pages); ?>
       </li>
     <?php } // while $nav_subjects 
     ?>
