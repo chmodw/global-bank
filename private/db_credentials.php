9550a26a2e24f995401ba80238d0f39b47f00678
chamodya wimansha <chamodyawimansha@gmail.com>
2021-01-15 10:45:00 +0530

    collapsing navigation added

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -2,5 +2,7 @@
 
 define("DB_SERVER", "localhost");
 define("DB_USER", "webuser");
-define("DB_PASS", "password");
+define("DB_PASS", "secretpassword");
 define("DB_NAME", "globe_bank");
+
+?>
--- public/index.php
+++ public/index.php
@@ -1,13 +1,25 @@
 <?php require_once('../private/initialize.php'); ?>
 
 <?php
-
-if (isset($_GET["id"])) {
-  $page_id = $_GET["id"];
+if(isset($_GET['id'])) {
+  $page_id = $_GET['id'];
   $page = find_page_by_id($page_id);
-  if (!$page) {
-    include(SHARED_PATH . '/static_homepage.php');
+  if(!$page) {
+    redirect_to(url_for('/index.php'));
   }
+  $subject_id = $page['subject_id'];
+} elseif(isset($_GET['subject_id'])) {
+  $subject_id = $_GET['subject_id'];
+
+  $page_set = find_pages_by_subject_id($subject_id);
+  $page = mysqli_fetch_assoc($page_set); // first page
+  mysqli_free_result($page_set);
+  if(!$page) {
+    redirect_to(url_for('/index.php'));
+  }
+  $page_id = $page['id'];
+} else {
+  // nothing selected; show the homepage
 }
 
 ?>
@@ -19,16 +31,23 @@ if (isset($_GET["id"])) {
   <?php include(SHARED_PATH . '/public_navigation.php'); ?>
 
   <div id="page">
+
     <?php
+      if(isset($page)) {
+        // show the page from the database
+        // TODO add html escaping back in
+        echo $page['content'];
 
-    if (isset($page)) {
-      // TODO add html
-      echo $page['content'];
-    } else {
-      include(SHARED_PATH . '/static_homepage.php');
-    }
-
+      } else {
+        // Show the homepage
+        // The homepage content could:
+        // * be static content (here or in a shared file)
+        // * show the first page from the nav
+        // * be in the database but add code to hide in the nav
+        include(SHARED_PATH . '/static_homepage.php');
+      }
     ?>
+
   </div>
 
 </div>
