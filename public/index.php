a3efea9157de920986bbaf749b41c5a0ebf26457
chamodya wimansha <chamodyawimansha@gmail.com>
2021-01-15 10:23:28 +0530

    loding page content

--- public/index.php
+++ public/index.php
@@ -1,5 +1,17 @@
 <?php require_once('../private/initialize.php'); ?>
 
+<?php
+
+if (isset($_GET["id"])) {
+  $page_id = $_GET["id"];
+  $page = find_page_by_id($page_id);
+  if (!$page) {
+    include(SHARED_PATH . '/static_homepage.php');
+  }
+}
+
+?>
+
 <?php include(SHARED_PATH . '/public_header.php'); ?>
 
 <div id="main">
@@ -7,7 +19,16 @@
   <?php include(SHARED_PATH . '/public_navigation.php'); ?>
 
   <div id="page">
+    <?php
 
+    if (isset($page)) {
+      // TODO add html
+      echo $page['content'];
+    } else {
+      include(SHARED_PATH . '/static_homepage.php');
+    }
+
+    ?>
   </div>
 
 </div>
