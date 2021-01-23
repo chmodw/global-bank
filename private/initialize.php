db1f754a3a9f6083f8e3c2c6f8ad3380424d9470
Chamodya Wimansha <chamodyawimansha@gmail.com>
2021-01-23 11:47:39 +0530

    require login

--- private/initialize.php
+++ private/initialize.php
@@ -2,7 +2,7 @@
   ob_start(); // output buffering is turned on
 
   session_start(); // turn on sessions
-  
+
   // Assign file paths to PHP constants
   // __FILE__ returns the current path to this file
   // dirname() returns the path to the parent directory
@@ -26,6 +26,7 @@
   require_once('database.php');
   require_once('query_functions.php');
   require_once('validation_functions.php');
+  require_once('auth_functions.php');
 
   $db = db_connect();
   $errors = [];
--- private/query_functions.php
+++ private/query_functions.php
@@ -334,6 +334,19 @@
     return $admin; // returns an assoc. array
   }
 
+  function find_admin_by_username($username) {
+    global $db;
+
+    $sql = "SELECT * FROM admins ";
+    $sql .= "WHERE username='" . db_escape($db, $username) . "' ";
+    $sql .= "LIMIT 1";
+    $result = mysqli_query($db, $sql);
+    confirm_result_set($result);
+    $admin = mysqli_fetch_assoc($result); // find first
+    mysqli_free_result($result);
+    return $admin; // returns an assoc. array
+  }
+
   function validate_admin($admin) {
 
     if(is_blank($admin['first_name'])) {
