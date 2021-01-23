c38751a9a15cfc0b5973088fcfde897ca2e8e46c
Chamodya Wimansha <chamodyawimansha@gmail.com>
2021-01-23 11:55:01 +0530

    chapter 6 started

--- private/auth_functions.php
+++ private/auth_functions.php
@@ -10,6 +10,16 @@
     return true;
   }
 
+  // Performs all actions necessary to log out an admin
+  function log_out_admin() {
+    unset($_SESSION['admin_id']);
+    unset($_SESSION['last_login']);
+    unset($_SESSION['username']);
+    // session_destroy(); // optional: destroys the whole session
+    return true;
+  }
+
+
   // is_logged_in() contains all the logic for determining if a
   // request should be considered a "logged in" request or not.
   // It is the core of require_login() but it can also be called
@@ -30,15 +40,6 @@
     } else {
       // Do nothing, let the rest of the page proceed
     }
-  }
-
-  // Performs all actions necessary to log out an admin
-  function log_out_admin() {
-    unset($_SESSION['admin_id']);
-    unset($_SESSION['last_login']);
-    unset($_SESSION['username']);
-    // session_destroy(); // optional: destroys the whole session
-    return true;
   }
 
 ?>
